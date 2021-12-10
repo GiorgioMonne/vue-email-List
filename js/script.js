61a2d0b728dfe8246e816e741804f9de1a35ff2e
Giorgio <1999giorgio.monne@mail.com>
2021-12-10 09:28:26 -1000

    inizio

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati. (modificato) 
+
+const app = new Vue ({
+    el: "#root",
+    data:{
+        emails:[
+        ]
+    },
+    methods:{
+    },
+    created(){
+
+        for(let i=1;i<10;i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // handle success
+                    this.emails.push(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+        }
+    }
+});
